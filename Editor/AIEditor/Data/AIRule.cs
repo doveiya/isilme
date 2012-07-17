09ac7ae679a0b166b34ab7cea949280635f1618a
velorth.avelies <velorth.avelies@gmail.com>
2012-07-17 15:59:05 +0000

    EDITOR: -DEL: Binary data

--- Editor/AIEditor/Data/AIRule.cs
+++ Editor/AIEditor/Data/AIRule.cs
@@ -2,20 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel;
 
 namespace MyApplication1.Data
 {
-    public class AIRule
+    /// <summary>
+    /// Model of AI rule
+    /// </summary>
+    public class AIRule : Common.ProxyObject
     {
+        #region Members
+        String mID;
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets identifier of the rule
+        /// </summary>
         public String ID
         {
-            get;
-            set;
+            get
+            {
+                return mID;
+            }
+            set
+            {
+                mID = value;
+                RaisePropertyChanged(this, "ID");
+            }
         }
+
+        #endregion
+
         AIAction Action;
         ActionTarget Target;
         Script Condition;
         Script Priority;
 
+        #region Constructors
+        #endregion
+
+        #region Methods
+        #endregion
     }
 }
